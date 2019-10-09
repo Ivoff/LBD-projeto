de7302f3dd2d9d9ccaa17024020f3e9fa1bcc83c
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-10-09 18:53:34 -0400

    Ajustado query de validacao de perfil

--- 03-function_validar_perfil.sql
+++ 03-function_validar_perfil.sql
@@ -5,19 +5,28 @@ CREATE OR REPLACE FUNCTION perfil_validation_maratona(
 DECLARE
     interacoesPerfilComMaratona INTEGER
 BEGIN
-    
     SELECT COUNT(*) AS quantidade
     INTO interacoesPerfilComMaratona
-    FROM Perfil
-    INNER JOIN Participante ON Participante.perfil_id = Perfil.id
-    INNER JOIN Membro ON Membro.perfil_id = Perfil.id
-    INNER JOIN Equipe ON Equipe.id = Membro.equipe_id
-    INNER JOIN EquipeMaratona ON EquipeMaratona.equipe_id = Equipe.id
-    WHERE 
-        Participante.maratona_id = maratonaId   AND
-        EquipeMaratona.maratona_id = maratonaId AND
-        Perfil.id = perfilId
-    GROUP BY Perfil.id
+    FROM (
+        (
+            SELECT Participante.perfil_id AS PerfilId
+            FROM Participante
+            WHERE
+                Participante.maratona_id = maratonaId   AND
+                Participante.perfil_id = perfilId
+        )
+        UNION
+        (
+            SELECT Membro.perfil_id AS PerfilId
+            FROM Membro
+            INNER JOIN Equipe ON Equipe.id = Membro.equipe_id
+            INNER JOIN EquipeMaratona ON EquipeMaratona.equipe_id = Equipe.id
+            WHERE 
+                EquipeMaratona.maratona_id = maratonaId AND
+                Membro.perfil_id = perfilId
+        )
+    ) AS UNIAO
+    GROUP BY UNIAO.PerfilId;
 
     IF interacoesPerfilComMaratona > 0 
     THEN
