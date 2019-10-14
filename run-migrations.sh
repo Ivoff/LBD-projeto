43b25a2e534a541e0e503ce977d3fd27dccea941
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-10-14 16:01:33 -0400

    Retirado sudo

--- run-migrations.sh
+++ run-migrations.sh
@@ -1,5 +1,4 @@
 . ./env
-sudo
 for MIGRATIONS in $(ls ./migrations);  
     do echo "Executando: ${MIGRATIONS}";
     PGPASSWORD=$DB_PASSWORD psql -h $HOST -d $DB_DATABASE -U $DB_USERNAME -p $PORT -a -w -f "./migrations/${MIGRATIONS}"
