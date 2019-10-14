20de41c7c44908b186c0b150ba96cf0d6baac7b2
ivoff <pyeroevo@gmail.com>
2019-10-14 09:44:48 -0400

    adicionando trigger para inserir o criador da equipe como membro

---
+++ migrations/08-trigger_add_membro_on_equipe_insert.sql
@@ -0,0 +1,14 @@
+create or replace function add_membro_equipe_on_insert() returns trigger as
+$$
+	declare
+	begin
+		insert into membro(perfil_id, equipe_id) values(new.criador_perfil_id, new.id);
+		return new;
+	end;
+$$ language plpgsql;
+
+/*create trigger add_membro_after_insert_on_equipe
+after insert
+on equipe
+for each row
+execute procedure add_membro_equipe_on_insert();*/
