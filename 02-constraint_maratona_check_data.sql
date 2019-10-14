bef2249582f01a014a53261d0d9c00d0ea87f033
ivoff <pyeroevo@gmail.com>
2019-10-14 09:00:46 -0400

    adicionando trigger de insercao de equipe na maratona e uns triggers de nome em maratona e equipe

--- 02-constraint_maratona_check_data.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE Maratona ADD CONSTRAINT maratona_check_data CHECK (
-   inscricao_comeco > NOW()
-   AND inscricao_termino >  inscricao_comeco 
-   AND horario_comeco > inscricao_termino
-   AND horario_termino > horario_comeco
-);
