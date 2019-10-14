f305b658ef3de3df92f38c174c0ae65d79ddd967
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-10-14 16:00:30 -0400

    Fix

--- run-migrations.sh
+++ run-migrations.sh
@@ -2,8 +2,6 @@
 sudo
 for MIGRATIONS in $(ls ./migrations);  
     do echo "Executando: ${MIGRATIONS}";
-    # $(cat ./migrations/${MIGRATIONS})
     PGPASSWORD=$DB_PASSWORD psql -h $HOST -d $DB_DATABASE -U $DB_USERNAME -p $PORT -a -w -f "./migrations/${MIGRATIONS}"
 done;
-echo "";
-echo "Migrations Executadas com sucesso!!!";
+echo "\nMigrations Executadas com sucesso!!!";
