5ac95703ba59ea913855108d0672d7d9d10b4191
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-10-14 15:55:55 -0400

    Adicionado script de migration

--- run-migrations.sh
+++ run-migrations.sh
@@ -1,5 +1,9 @@
 . ./env
+sudo
 for MIGRATIONS in $(ls ./migrations);  
-    do echo ${MIGRATIONS};
-    psql postgres -c "cat ${MIGRATIONS}" --host=$HOST --port=$PORT -d $DB_DATABASE -U $DB_USERNAME --password=$DB_PASSWORD;
+    do echo "Executando: ${MIGRATIONS}";
+    # $(cat ./migrations/${MIGRATIONS})
+    PGPASSWORD=$DB_PASSWORD psql -h $HOST -d $DB_DATABASE -U $DB_USERNAME -p $PORT -a -w -f "./migrations/${MIGRATIONS}"
 done;
+echo "";
+echo "Migrations Executadas com sucesso!!!";
