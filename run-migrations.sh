f107e34e6f2a48cc14c0a687a0b70157e3ac11ac
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-10-15 14:06:21 -0400

    Senha automatica

--- run-migrations.sh
+++ run-migrations.sh
@@ -18,7 +18,7 @@ done
 if [[ $DROP != 'nope' ]]; then
     echo -e "${green}Dropando o public schema${reset}"
     DROP_COMMAND="DROP SCHEMA public CASCADE;CREATE SCHEMA public;"
-    psql -h $HOST -d $DB_DATABASE -U $DB_USERNAME -p $PORT -c "$DROP_COMMAND"
+    PGPASSWORD=$PGPASSWORD psql -h $HOST -d $DB_DATABASE -U $DB_USERNAME -p $PORT -c "$DROP_COMMAND"
 fi
 
 for MIGRATION in migrations/*; do
@@ -29,7 +29,7 @@ for MIGRATION in migrations/*; do
     fi
 
     echo -e "${green}Executando${reset}: ${MIGRATION}";
-    psql -v ON_ERROR_STOP=1 -h $HOST -d $DB_DATABASE -U $DB_USERNAME -p $PORT -f "$MIGRATION"
+    PGPASSWORD=$PGPASSWORD psql -v ON_ERROR_STOP=1 -h $HOST -d $DB_DATABASE -U $DB_USERNAME -p $PORT -f "$MIGRATION"
     if [ $? -ne 0 ]; then
         echo -e "${red}Encerrando devido a erro${reset}";
         break;
