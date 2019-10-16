1057815d924363af2a93ddd533d21d70b01388f6
ivoff <pyeroevo@gmail.com>
2019-10-15 23:34:04 -0400

    .

--- migrations/00-ddl.sql
+++ migrations/00-ddl.sql
@@ -70,11 +70,11 @@ create table EquipeMaratona(
 
 
 create table EquipeMaratonaQuestao(
-	id serial primary key,
+    id serial primary key,
     maratona_questoes_id int references MaratonaQuestoes(id) not null,
-	equipe_id int references equipe(id) not null,
-	maratona_id int references maratona(id) not null,
-    pontuacao_final double precision not null DEFAULT 0,
+    equipe_id int references equipe(id) not null,
+    maratona_id int references maratona(id) not null,
+    pontuacao double precision not null DEFAULT 0,
     tentativas int not null  DEFAULT 0,
     inscricao_termino timestamp with time zone not null DEFAULT CURRENT_TIMESTAMP
 );
