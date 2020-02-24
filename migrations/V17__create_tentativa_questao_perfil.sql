312c8968651eba35cc184a854c283ee89be2bf17
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2020-02-24 18:38:01 -0400

    Adicionado porcentagem

--- migrations/V17__create_tentativa_questao_perfil.sql
+++ migrations/V17__create_tentativa_questao_perfil.sql
@@ -1,9 +1,10 @@
 create table TentativaQuestao
 (
-    id         serial primary key,
-    perfil_id  integer                  not null references Perfil (id),
-    questao_id integer                  not null references QuestaoId (id),
-    status     int                      not null,
-    entrada    text                     not null,
-    criado_em  timestamp with time zone not null DEFAULT CURRENT_TIMESTAMP
+    id               serial primary key,
+    perfil_id        integer                  not null references Perfil (id),
+    questao_id       integer                  not null references QuestaoId (id),
+    status           int                      not null,
+    entrada          text                     not null,
+    criado_em        timestamp with time zone not null DEFAULT CURRENT_TIMESTAMP,
+    porcentagem_erro int                      null
 );
