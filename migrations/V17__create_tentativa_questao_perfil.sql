80dccd50c8618b658a1d1ac4e452eebed6524bec
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2020-02-24 23:34:44 -0400

    Fix

--- migrations/V17__create_tentativa_questao_perfil.sql
+++ migrations/V17__create_tentativa_questao_perfil.sql
@@ -1,4 +1,4 @@
-create table TentativaQuestao
+create table tentativaQuestao
 (
     id               serial primary key,
     perfil_id        integer                  not null references Perfil (id),
@@ -6,5 +6,6 @@ create table TentativaQuestao
     status           int                      not null,
     entrada          text                     not null,
     criado_em        timestamp with time zone not null DEFAULT CURRENT_TIMESTAMP,
-    porcentagem_erro int                      null
+    porcentagem_erro int                      null,
+    saida_erro       text                     null
 );
